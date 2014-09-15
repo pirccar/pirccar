c8d0489df4dca7620bd539dd5e9a768c6a19ea28
murta <martin.rydberg@gmail.com>
2014-09-15 13:53:53 +0200

    Added code and comments
    - Added code to servocontrol.cpp/h
    - Added comments about pin numbering in lcsdscreen.cpp

---
+++ carserver/servocontrol.cpp
@@ -0,0 +1,16 @@
+#include "servocontrol.h"
+
+void ServoControl::init()
+{
+	
+}
+
+void ServoControl::setServoPosition(char servo, int pos)
+{
+}
+
+int ServoControl::setServoPosition(char servo)
+{
+	int servoPosition = 0;
+	return servoPosition;
+}
