4fddc11816b19b34593020e24b6266a304862465
Fredrik Olsson <fredrik.o.olsson@afconsult.com>
2014-10-21 15:06:51 +0200

    Revert "Revert "New Gohom feature""
    This reverts commit 234777d83c3b34fdc0f8209aa3cc8eb482629149.

---
+++ RCClient/RCClient/ResolutionIndependence/Camera2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace RCClient.ResolutionIndependence
+{
+    class Camera2D
+    {
+        private float zoom;
+        private float rotation;
+        private Vector2 position;
+        private Matrix transform = Matrix.Identity;
+        private bool isViewTransformationDity = true;
+        private Matrix camTranslationMatrix = Matrix.Identity;
+        private Matrix camRotationMatrix = Matrix.Identity;
+        private Matrix camScaleMatrix = Matrix.Identity;
+        private Matrix resTranslationMatrix = Matrix.Identity;
+
+        protected ResolutionIndependentRenderer resolutionIndependentRenderer;
+        private Vector3 camTranslationVector = Vector3.Zero;
+        private Vector3 camScaleVector = Vector3.Zero;
+        private Vector3 resTranslationVector = Vector3.Zero;
+
+
+    }
+}
